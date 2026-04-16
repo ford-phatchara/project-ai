f7fe99aef5e903a7993994b3bf59931c83b650d1
Phatchara Sangsuphap <phatchara.s@orcsoft.co.th>
2026-04-16 17:16:51 +0700

    change from css to scss

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('frontend');
